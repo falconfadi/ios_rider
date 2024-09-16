df1a53f020489d677b02eaf27cd7ed5c9b742831
Fadi frejat <fadi.alfrejat@gmail.com>
2024-09-16 15:43:42 +0300

    first commit

---
+++ www/js/lang/langs.js
@@ -0,0 +1,10 @@
+var available_langs = [
+    {code:'en',name:'English',dir:'ltr'},
+    {code:'hi',name:'Hindi (हिंदी)',dir:'ltr'},
+    {code:'es',name:'Spanish (Español)',dir:'ltr'},
+    {code:'fr',name:'French (français)',dir:'ltr'},
+    {code:'de',name:'Deutsch',dir:'ltr'},
+    {code:'ar',name:'Arabic (عربي)',dir:'rtl'},
+    {code:'ru',name:'Russian (русский)',dir:'ltr'},
+    {code:'pt',name:'Portuguese (português)',dir:'ltr'}
+];
